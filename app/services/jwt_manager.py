b16cdb15fdeaf7a0f109c7bd50fb65ceeb9417bf
Peskov Sergey <peskovdev@proton.me>
2023-06-24 22:54:43 +0600

    Fix bug with Authorization: "token"

--- app/services/jwt_manager.py
+++ app/services/jwt_manager.py
@@ -14,6 +14,11 @@ from app.schemas.login import Token
 def get_token_payload(token: str = Security(APIKeyHeader(name="Authorization"))) -> Token:
     """Validates token and returns decoded Token"""
     try:
+        if token[-1] == '"':
+            token = token[:-1]
+        if token[0] == '"':
+            token = token[1:]
+
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         if payload.get("id") is None:
             raise HTTPException(status_code=400, detail="Invalid token: missing id")
